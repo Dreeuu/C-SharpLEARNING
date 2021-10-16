95a3f9cc169f290b750755789e88c302316fbf3a
Dreeuu <maciej.g2004@gmail.com>
2021-10-16 16:18:34 +0200

    Add 2 program

---
+++ 2.ZnakiSpecjalne/2.ZnakiSpecjalne/2.ZnakiSpecjalne/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2.ZnakiSpecjalne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("POLICE\aLapy do góry");
+
+        }
+    }
+}
