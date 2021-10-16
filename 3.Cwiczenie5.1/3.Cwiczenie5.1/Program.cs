4a200b83de4cca7fe7c7386b02344b754b47a518
Dreeuu <maciej.g2004@gmail.com>
2021-10-16 16:52:43 +0200

    Add Cwiczenie5.1

---
+++ 3.Cwiczenie5.1/3.Cwiczenie5.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _3.Cwiczenie5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short shorty=4500;
+            short spodnie=820;
+
+           
+        }
+    }
+}
