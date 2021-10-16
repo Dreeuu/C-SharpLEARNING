2bc08d3aa0e8e460018f5487973c5c2b62b623a9
Dreeuu <maciej.g2004@gmail.com>
2021-10-16 17:00:04 +0200

    Add Cwiczenie 5.2

---
+++ 4.Cwiczenie5.2/4.Cwiczenie5.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _4.Cwiczenie5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test co sie stanie gdy prrzypisze typowi byte wartosc spoza jego zakresu
+            byte liczba = 255;
+            sbyte liczba2 = 129;
+            byte liczba3 = 256;
+            Console.WriteLine(liczba);
+            Console.WriteLine(liczba2);
+            Console.WriteLine(liczba3);
+        }
+    }
+}
