ec34267bd78b788af099904fc802b2f8c834b800
Dreeuu <maciej.g2004@gmail.com>
2021-10-16 15:33:09 +0200

    Add XML

--- 1.HelloWorld/1.HelloWorld/Program.cs
+++ 1.HelloWorld/1.HelloWorld/Program.cs
@@ -2,10 +2,15 @@
 
 namespace _1.HelloWorld
 {
+ /// <summary>
+ /// Główna klasa apliakcji
+ /// </summary>
     class Program
-    {
+    {   
+        ///<remarks>Metoda startowa aplikacji</remarks>
         static void Main(string[] args)
         {
+            ///<remarks>xd</remarks>
             Console.WriteLine("Hellow World!");
         }
     }
