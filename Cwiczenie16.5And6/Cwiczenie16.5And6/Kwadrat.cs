316acc140de9cecf092cf98974a3f51da4360450
Maciej <maciej.g2004@gmail.com>
2021-10-17 19:34:47 +0200

    16.5And16.6

---
+++ Cwiczenie16.5And6/Cwiczenie16.5And6/Kwadrat.cs
@@ -0,0 +1,35 @@
+﻿
+
+public class Kwadrat
+{
+    int xP, yP;
+    int xL, yL;
+    int dlugoscBoku;
+    int xS, yS;
+    public Kwadrat(int xP, int yP, int xL, int yL)
+    {
+        this.xP = xP;
+        this.yP = yP;
+        this.xL = xL;
+        this.yL = yL;
+    }
+    public Kwadrat(int xL,int yL,int dlugoscBoku)
+    {
+        this.xL = xL;
+        this.yL = yL;
+        this.dlugoscBoku = dlugoscBoku;
+    }
+    public Kwadrat():this(5,5,5)
+    {
+        this.xS = xL;
+        this.yS = yL;
+        this.dlugoscBoku = dlugoscBoku;
+
+    }
+    public void wyswietlenie()
+    {
+        System.Console.WriteLine("Wspolrzedne wierzcholka prawego: [{0},{1}]",xP,yP);
+        System.Console.WriteLine("Wspolrzedne wierzcholka lewego: [{0},{1}]",xL ,yL);
+    }
+}
+
---
+++ Cwiczenie16.5And6/Cwiczenie16.5And6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int xP=5, yP=5, szerokosc=5, wysokosc=5;
+        
+
+        Kwadrat k1 = new Kwadrat(xP,yP,szerokosc,wysokosc);
+
+        k1.wyswietlenie();
+        Kwadrat k2 = new Kwadrat(xP, yP, szerokosc);
+        k2.wyswietlenie();
+        }
+    }
+
